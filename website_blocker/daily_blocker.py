fa4feba3d8956158c666966ea2acff5d1a77f823
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 18:01:54 -0400

    initialized the program file

---
+++ website_blocker/daily_blocker.py
@@ -0,0 +1,7 @@
+
+# Open the hosts file from C:\Windows\System32\drivers\etc
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com", "facebook.com", "www.linkedin.com", "linkedin.com"]
+
+# check if it is work hours or off-work hours
