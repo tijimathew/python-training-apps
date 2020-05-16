2adbb9556b9abd3913bf61fd9a10a7257bd5e2f8
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 15:50:48 -0400

    Mapping layers completed.

--- mappings/map1.py
+++ mappings/map1.py
@@ -27,14 +27,21 @@ def color_producer(elevation):
 
 map = folium.Map(location=[lat[0], lon[0]], zoom_start=8, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, vel, vnam, vtyp in zip(lat, lon, vol_elev, vol_name, vol_type):
     iframe = folium.IFrame(html=html % (vnam, vnam, vtyp, str(vel)), width=300, height=200)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=folium.Popup(iframe), fill_color=color_producer(vel), color='grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=folium.Popup(iframe), 
+    fill_color=color_producer(vel), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fgp = folium.FeatureGroup(name="Population")
 
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl()) # add layer control after adding feature group to map.
 
 map.save("map_advanced_color.html")
