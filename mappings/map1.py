96e84b360495a08b81e44035ed51522aa2bbf1c8
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 10:16:07 -0400

    reading coordinates from data file and placing points on the map.

--- mappings/map1.py
+++ mappings/map1.py
@@ -1,11 +1,17 @@
 import folium
+import pandas as pd
 mylocation = [34.133159, -84.144573]
+data = pd.read_csv('volcanoes.txt')
+
+# Create two lists - one with latitudes and one with longitudes
+lat = list(data['LAT'])
+lon = list(data['LON'])
 
 map = folium.Map(location=mylocation, zoom_start=8, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-for coordinates in [[34.1, 15.1],[36.1, 17.1],[34.133159, -84.144573]]:
-    fg.add_child(folium.Marker(location=coordinates , popup="Coordinates : %s, %s " % (coordinates[0], coordinates[1]), icon=folium.Icon(color='pink')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Coordinates : %s, %s " % (lt, ln), icon=folium.Icon(color='pink')))
 
 
 map.add_child(fg)
