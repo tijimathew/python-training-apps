5dfb59374f80f828ee13aafe36ebb41760066792
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 18:06:46 -0400

    added frequency value

--- website_blocker/daily_blocker.py
+++ website_blocker/daily_blocker.py
@@ -1,7 +1,10 @@
-
+import time
 # Open the hosts file from C:\Windows\System32\drivers\etc
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
-redirect="127.0.0.1"
-website_list=["www.facebook.com", "facebook.com", "www.linkedin.com", "linkedin.com"]
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com", "www.linkedin.com", "linkedin.com"]
 
-# check if it is work hours or off-work hours
+while True:
+    # continously check if it is work hours or off-work hours
+    print(1)
+    time.sleep(5)
