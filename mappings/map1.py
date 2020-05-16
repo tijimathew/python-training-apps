b5071894f670b55bab6936581fef6ca6bad4904d
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 10:00:14 -0400

    adding tooltips and looping through coorodinates example

--- mappings/map1.py
+++ mappings/map1.py
@@ -1,10 +1,13 @@
 import folium
 mylocation = [34.133159, -84.144573]
 
-map = folium.Map(location=mylocation, zoom_start=14, tiles="Stamen Terrain")
+map = folium.Map(location=mylocation, zoom_start=8, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=mylocation, popup="Close to Home", icon=folium.Icon(color='pink')))
+for coordinates in [[34.1, 15.1],[36.1, 17.1],[34.133159, -84.144573]]:
+    fg.add_child(folium.Marker(location=coordinates , popup="Coordinates : %s, %s " % (coordinates[0], coordinates[1]), icon=folium.Icon(color='pink')))
+
+
 map.add_child(fg)
 
 map.save("map1.html")
