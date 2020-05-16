84206863274984a358eb4e91470b3e670cc2311a
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 10:58:07 -0400

    advaned coloring of icons on the map

--- mappings/map1.py
+++ mappings/map1.py
@@ -4,25 +4,35 @@ import pandas as pd
 data = pd.read_csv('volcanoes.txt')
 
 html = """
-Volcano Name: 
+<h4>Volcano Name:</h4> 
 <a href="https://www.google.com/search?q=%%22Volcanoes%%22+%%22%s%%22" target="_blank">%s</a><br>
-Type: %s
+Type: %s<br>
+Elevation: %s
 """
 
 # Create two lists - one with latitudes and one with longitudes
 lat = list(data['LAT'])
 lon = list(data['LON'])
+vol_elev = list(data['ELEV'])
 vol_name = list(data['NAME'])
 vol_type = list(data['TYPE'])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif elevation >=1000 and elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[lat[0], lon[0]], zoom_start=8, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln, vnam, vtyp in zip(lat, lon, vol_name, vol_type):
-    iframe = folium.IFrame(html=html % (vnam, vnam, vtyp), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='pink')))
+for lt, ln, vel, vnam, vtyp in zip(lat, lon, vol_elev, vol_name, vol_type):
+    iframe = folium.IFrame(html=html % (vnam, vnam, vtyp, str(vel)), width=300, height=200)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(vel))))
 
 
 map.add_child(fg)
 
-map.save("map_advanced.html")
+map.save("map_advanced_color.html")
