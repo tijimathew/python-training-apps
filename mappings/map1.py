2c1c65638926dddd7128b1e1caf31ce013f87bf2
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 09:30:29 -0400

    added point to map

--- mappings/map1.py
+++ mappings/map1.py
@@ -1,3 +1,6 @@
 import folium
 map = folium.Map(location=[34, 15], zoom_start=6, tiles="Stamen Terrain")
+
+map.add_child(folium.Marker(location=[34.1, 15.1], popup="Hi I am a marker", icon=folium.Icon(color='green')))
+
 map.save("map1.html")
