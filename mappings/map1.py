b1ea957511382a5421ce8058201a35024aa4cc45
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 10:44:37 -0400

    Added dynamic HTML content to the popup to search the details on google.

--- mappings/map1.py
+++ mappings/map1.py
@@ -3,21 +3,26 @@ import pandas as pd
 #mylocation = [34.133159, -84.144573]
 data = pd.read_csv('volcanoes.txt')
 
+html = """
+Volcano Name: 
+<a href="https://www.google.com/search?q=%%22Volcanoes%%22+%%22%s%%22" target="_blank">%s</a><br>
+Type: %s
+"""
+
 # Create two lists - one with latitudes and one with longitudes
 lat = list(data['LAT'])
 lon = list(data['LON'])
-vol_num = list(data['NUMBER'])
 vol_name = list(data['NAME'])
 vol_type = list(data['TYPE'])
 
 map = folium.Map(location=[lat[0], lon[0]], zoom_start=8, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln, vnum, vnam, vtyp in zip(lat, lon, vol_num, vol_name, vol_type):
-    popup_lbl = "<strong>Number</strong>: %s<br>Name: %s<br>Type: %s" % (str(vnum), vnam, vtyp)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=popup_lbl, icon=folium.Icon(color='pink')))
+for lt, ln, vnam, vtyp in zip(lat, lon, vol_name, vol_type):
+    iframe = folium.IFrame(html=html % (vnam, vnam, vtyp), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='pink')))
 
 
 map.add_child(fg)
 
-map.save("map1.html")
+map.save("map_advanced.html")
