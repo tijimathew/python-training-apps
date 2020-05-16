237725798ff2af837e372670399e32c7fc11f52a
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-16 10:31:21 -0400

    updated popup with dynamic labels

--- mappings/map1.py
+++ mappings/map1.py
@@ -1,17 +1,21 @@
 import folium
 import pandas as pd
-mylocation = [34.133159, -84.144573]
+#mylocation = [34.133159, -84.144573]
 data = pd.read_csv('volcanoes.txt')
 
 # Create two lists - one with latitudes and one with longitudes
 lat = list(data['LAT'])
 lon = list(data['LON'])
+vol_num = list(data['NUMBER'])
+vol_name = list(data['NAME'])
+vol_type = list(data['TYPE'])
 
-map = folium.Map(location=mylocation, zoom_start=8, tiles="Stamen Terrain")
+map = folium.Map(location=[lat[0], lon[0]], zoom_start=8, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Coordinates : %s, %s " % (lt, ln), icon=folium.Icon(color='pink')))
+for lt, ln, vnum, vnam, vtyp in zip(lat, lon, vol_num, vol_name, vol_type):
+    popup_lbl = "<strong>Number</strong>: %s<br>Name: %s<br>Type: %s" % (str(vnum), vnam, vtyp)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=popup_lbl, icon=folium.Icon(color='pink')))
 
 
 map.add_child(fg)
