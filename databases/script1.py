c604fd425d239e852256ccae1aa0da6a07fceba1
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-18 11:36:32 -0400

    create, insert and fetch rows sqllite3

--- databases/script1.py
+++ databases/script1.py
@@ -7,8 +7,29 @@
 
 import sqlite3
 
-conn = sqlite3.connect("lite.db")
-cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-conn.commit()
-conn.close()
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+create_table()
+insert("Coffee Cup", 5, 5.5)
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
